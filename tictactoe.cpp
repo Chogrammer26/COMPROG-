59da6997f29b06a18b7af605768c980ee996abee
z-zz-zzz <54175085+z-zz-zzz@users.noreply.github.com>
2019-10-29 17:07:09 +0800

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -44,9 +44,9 @@ void ticGame(){
 	cout << "|_______________________________________________________________|";
 	cin.ignore();
 	cout << "|                          Tic-tac-toe                          |\n";	
-	cout << "|   (American English), noughts and crosses British English),   |\n";	
+	cout << "|   (American English), noughts and crosses (British English),  |\n";	
 	cout << "|   or Xs and Os is a paper-and-pencil game for two players,    |\n";	
-	cout << "|   X and O, who take turns marking the spaces in a 3×3 grid.   |\n";	
+	cout << "|   X and O, who take turns marking the spaces in a 3Ã—3 grid.   |\n";	
 	cout << "|   The player who succeeds in placing three of their marks in  |\n";	
 	cout << "|   a horizontal, vertical, or diagonal row is the winner.      |\n";	
 	cout << "|                                                               |\n";		
